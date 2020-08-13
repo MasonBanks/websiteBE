00bb341632158b94dfc400329c917a309a738cd8
Mason Banks <masonbanks@ymail.com>
2020-08-13 11:41:06 +0100

    DB connection added

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const MongoClient = require('mongodb');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('dotenv/config');
 
 const app = express();
 
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-require('./app/routes')(app, {});
+app.use(bodyParser.json());
+
+require('./app/routes/')(app, {});
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => {
+    console.log('Connected to DB')
+  }
+)
 
 app.listen(port, () => {
   console.log(`We are live on port ${port}`)
